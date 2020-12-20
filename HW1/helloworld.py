96fb26429a3a6f8ea66def62a466880685b9cbd5
Sai Kathika <32946929+Saiprem98@users.noreply.github.com>
2020-12-19 17:19:07 -0800

    Add files via upload

---
+++ HW1/helloworld.py
@@ -0,0 +1,14 @@
+# CS 190DD
+# HW 1
+
+# Problem 1 
+
+# Problem 1
+# Increase your familiarity with Github. This problem needs to be finished with a partner. Follow these steps to finish the problem:
+
+# Create a github account.
+# Create a public repository and push a helloworld.py file.
+# Person  𝐴  forks Person  𝐵 's repo and modifies the file, then pushes to their own fork and finally creates a pull request for person B to merge.
+# Person  𝐵  reviews and merges the pull request.
+# Do the same for  𝐴  and  𝐵  reversed.
+# Provide the links to your repo and your partner's repo.
